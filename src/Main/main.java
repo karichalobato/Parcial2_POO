ada4e8ed6ffc9cc53545e81e50b324e357838521
Karicha Lobato <00002517@uca.edu.sv>
2018-06-11 09:17:30 -0600

    menu

--- src/Main/main.java
+++ src/Main/main.java
@@ -13,10 +13,10 @@ public class main {
     public static void main(String[] args){
        
      
-        Menu motor = Menu.getInstance();
+        Motor menu = Motor.getInstance();
          
         
-        motor.opcionesiniciales();
+        menu.mostrar();
       
          
          
