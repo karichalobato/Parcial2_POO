ac9d80bab597f97c63668cfbecc72dc1185a38e7
00002517@uca.edu.sv <UCA@DEILAB02-PC11>
2018-06-11 20:31:35 -0600

    creando especialistas

--- src/Milicias/Especialistas.java
+++ src/Milicias/Especialistas.java
@@ -7,6 +7,68 @@ package Milicias;
  *
  * @author Karicha Valesska Romero Lobato <00002517@uca.edu.sv>
  */
-public class Especialistas {
+public class Especialistas implements milicia {
+    private int vida,ataque;
+    private String nombre;
+
+    public Especialistas(miliciaBuilder Builder) {
+        this.vida = Builder.vida;
+        this.ataque = Builder.ataque;
+        this.nombre = Builder.nombre;
+    }
     
+    
+    @Override
+    public int getVida() {
+        return vida;
+    }
+
+    @Override
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    @Override
+    public int getAtaque() {
+        return ataque;
+    }
+
+    @Override
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public static class miliciaBuilder{
+        private int vida,ataque;
+        private String nombre;
+    
+    public miliciaBuilder vida(int vida){
+        this.vida=vida;
+        return this ;
+    }
+     
+    public miliciaBuilder ataque(int ataque){
+        this.ataque=ataque;
+        return this ;
+    }
+    
+    public miliciaBuilder nombre(String nombre){
+        this.nombre=nombre;
+        return this ;
+    }
+    public Especialistas build(){
+        return new Especialistas(this);
+    }
+    
+    }
 }
