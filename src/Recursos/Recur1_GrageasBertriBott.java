9281c15cb11f05441a385b4bef04e8bc2aa45aa5
00002517@uca.edu.sv <>
2018-06-11 21:19:06 -0600

    Creando recursos

---
+++ src/Recursos/Recur1_GrageasBertriBott.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Recursos;
+
+/**
+ *
+ * @author UCA
+ */
+public class Recur1_GrageasBertriBott implements recursos {
+    private String nombre;
+    private int cantidad;
+
+    public Recur1_GrageasBertriBott(recursos_Builder Builder) {
+        this.nombre = Builder.nombre;
+        this.cantidad = Builder.cantidad;
+    }
+    
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    public static class recursos_Builder{
+        private int cantidad;
+        private String nombre;
+    
+     
+    public recursos_Builder cantidad(int cantidad){
+        this.cantidad=cantidad;
+        return this ;
+    }
+    
+    public recursos_Builder nombre(String nombre){
+        this.nombre=nombre;
+        return this ;
+    }
+    
+    public Recur1_GrageasBertriBott build(){
+        return new Recur1_GrageasBertriBott(this);
+    }
+    
+    }
+    
+}
---
+++ src/Recursos/Recur2_CortezaDeVital.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Recursos;
+
+/**
+ *
+ * @author UCA
+ */
+public class Recur2_CortezaDeVital implements recursos {
+    
+    
+}
