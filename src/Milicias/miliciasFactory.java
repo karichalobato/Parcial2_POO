6f947310d83e269ec93d93f62b77bb1b5c267d0f
Karicha Lobato <00002517@uca.edu.sv>
2018-06-13 21:31:15 -0600

    Nombre a escuadrones y especialistas

--- src/Milicias/miliciasFactory.java
+++ src/Milicias/miliciasFactory.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Factory de milicias
  */
 package Milicias;
 
@@ -11,31 +9,32 @@ package Milicias;
  */
 public class miliciasFactory {
     public static milicia getMilicia(String opcion,String miliciA){
+        System.out.println(opcion);
         switch(opcion){
-            case "GRIFFINDOR":
+            case "GRYFFINDOR":
                 switch (miliciA){
                     case "ESCUADRONES":
-                        return new Escuadrones.miliciaBuilder().nombre("GrageasBertriBott").ataque(10).vida(100).Builder();
-                    case "ESPECIALISTA":
-                        return new Especialistas.miliciaBuilder().nombre("CortezaDeArbolVital").ataque(20).vida(50).Builder();
+                        return new Escuadrones.miliciaBuilder().nombre("La orden del fenix").ataque(10).vida(100).Builder();
+                    case "ESPECIALISTAS":
+                        return new Especialistas.miliciaBuilder().nombre("Fenix").ataque(20).vida(50).Builder();
                    
                 }
             
-            case "SLYTHERIN":
+            case "SLYTHRIN":
                 switch (miliciA){
                     case "ESCUADRONES":
-                        return new Escuadrones.miliciaBuilder().nombre("GrageasBertriBott").ataque(10).vida(100).Builder();
+                        return new Escuadrones.miliciaBuilder().nombre("Mortifagos").ataque(10).vida(100).Builder();
                     case "ESPECIALISTAS":
-                        return new Especialistas.miliciaBuilder().nombre("CortezaDeArbolVital").ataque(20).vida(50) .Builder();
+                        return new Especialistas.miliciaBuilder().nombre("Basilisco").ataque(20).vida(50) .Builder();
                     
                 }
             
             case "RAVENCLOW":
                 switch (miliciA){
                     case "ESCUADRONES":
-                        return new Escuadrones.miliciaBuilder().nombre("GrageasBertriBott").ataque(10).vida(100).Builder();
-                    case "ESPECIALISTA":
-                        return new Especialistas.miliciaBuilder().nombre("CortezaDeArbolVital").ataque(20).vida(50).Builder();
+                        return new Escuadrones.miliciaBuilder().nombre("Los guerreros Dumbledore").ataque(10).vida(100).Builder();
+                    case "ESPECIALISTAS":
+                        return new Especialistas.miliciaBuilder().nombre("Eagle").ataque(20).vida(50).Builder();
                     
                 }
         }
